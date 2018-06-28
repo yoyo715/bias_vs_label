ac87c1a00fcf3576f0f706379a3eaf34556dc1b1
madi <mcooley3@uwyo.edu>
2018-06-28 15:35:53 -0600

    worked on softmax

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from dictionary import Dictionary
 import numpy as np
 from scipy import sparse
+from numpy.linalg import inv
 
 
 # update rule for weight matrix B
@@ -13,19 +14,24 @@ def update_b(B, A, x, labels, nlabels):
     Sn = 0
     A_xn = sparse.csr_matrix.dot(A, x.T)  
     while k <= nlabels:
-        product = np.dot(B[:, k-1].T, A_xn)
+        product = np.dot(B[:, k-1], A_xn)
         Sn += np.dot(product, nlabels)
         k += 1
 
     K = 1
-    BAxn = np.dot(B.T, A_xn)
+    BAxn = np.dot(B, A_xn)
     while K <= nlabels:
         # create I_k
         I_k = np.ones(nlabels)
         I_k[K-1] = 0    # kth element set to 0
 
-        num = np.dot(I_k, BAxn) 
-        denom = np.dot(B[:, K-1].T, A_xn)
+        #num = np.dot(I_k, BAxn) 
+        #enom = np.dot(B[:, K-1].T, A_xn)
+        
+        IkY = np.dot(labels, I_k.T)
+        
+        #B_KAxn = 1 / np.dot(B[:, K-1].T, A_xn)  # ??
+        #print(B_KAxn)
         
         K += 1
         
@@ -36,11 +42,19 @@ def update_a():
 
 
 # calculates softmax value
-def softmax(x, A, B, nlabels):
+#def softmax(x, A, B, nlabels):
+    #temp = sparse.csr_matrix.dot(A, x.T)  
+    #X = np.dot(B.T, temp)
+    #exp = np.exp(product)
+    #return exp / nlabels * exp
+
+
+def stable_softmax(x, A, B):
     temp = sparse.csr_matrix.dot(A, x.T)  
-    product = np.dot(B.T, temp)
-    exp = np.exp(product)
-    return exp / nlabels * exp
+    X = np.dot(B, temp)
+    #print(X.shape)
+    exps = np.exp(X - np.max(X))
+    return exps / np.sum(exps)
     
 
 # calculates log-likelihood
@@ -82,11 +96,13 @@ def main():
     A_m = nwords + BUCKET   # cols
     uniform_val = 1.0 / DIM
     A = np.random.uniform(-uniform_val, uniform_val, (A_n, A_m))
+    #print(A.shape)
 
     # B
-    B_n = DIM               # rows
-    B_m = nlabels           # cols
+    B_n = nlabels           # rows
+    B_m = DIM               # cols
     B = np.zeros((B_n, B_m))
+    #print(B.shape)
 
     #### train ################################################
 
@@ -96,11 +112,16 @@ def main():
     loss = 0
     l = 0
     for x in input_:
-        loglike = log_likelihood(x, A, B, nlabels)
-        loss = loss_function(labels[l], loglike)
-        total_loss += loss        
+        #print(x.shape)
+        #loglike = log_likelihood(x, A, B, nlabels)
+        softmax = stable_softmax(x, A, B)
+        #print(softmax.shape)
+        #loss = loss_function(labels[l], loglike)
+        #total_loss += loss        
 
-        B_new = update_b(B, A, x, labels[l], nlabels)
+        #B_new = update_b(B, A, x, labels[l], nlabels)
+        #temp = sparse.csr_matrix.dot(A, x.T)
+        #print(temp)
         
         l += 1
         
