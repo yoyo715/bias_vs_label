eb8f21731f97e914291c3d640a091d3fa8c500ba
madi <mcooley3@uwyo.edu>
2018-07-06 08:53:50 -0600

    updating B

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#main.py
+# main.py
 
 from dictionary import Dictionary
 import numpy as np
@@ -6,34 +6,24 @@ from scipy import sparse
 from numpy.linalg import inv
 
 
-# update rule for weight matrix B
-# 
-def update_b(B, A, x, labels, nlabels):
-    # create S
-    k =1 
-    Sn = 0
-    A_xn = sparse.csr_matrix.dot(A, x.T)  
-    while k <= nlabels:
-        product = np.dot(B[:, k-1], A_xn)
-        Sn += np.dot(product, nlabels)
-        k += 1
-
-    K = 1
-    BAxn = np.dot(B, A_xn)
-    while K <= nlabels:
-        # create I_k
-        I_k = np.ones(nlabels)
-        I_k[K-1] = 0    # kth element set to 0
-
-        #num = np.dot(I_k, BAxn) 
-        #enom = np.dot(B[:, K-1].T, A_xn)
+def gradient_B(B, A, x, label, nlabels, alpha):
+    j = 0
+    xA = sparse.csr_matrix.dot(x, A)
+    y_hat = stable_softmax(x, A, B)
+    while j < nlabels:
+        Bj = B[j, :]
+        yj_hat = y_hat[j]
+        yj = label[j]
         
-        IkY = np.dot(labels, I_k.T)
+        Bj_new = (yj_hat - yj) * xA
+        #Bj_new = Bj_new / np.linalg.norm(Bj_new)
         
-        #B_KAxn = 1 / np.dot(B[:, K-1].T, A_xn)  # ??
-        #print(B_KAxn)
-        
-        K += 1
+        B[j,:] = B[j,:] + alpha * Bj_new
+        j += 1
+    
+    B = B / np.linalg.norm(B)
+    #print(B)
+    return B
         
 
 # update rule for weight matrix A
@@ -41,37 +31,29 @@ def update_a():
     return 1
 
 
-# calculates softmax value
-#def softmax(x, A, B, nlabels):
-    #temp = sparse.csr_matrix.dot(A, x.T)  
-    #X = np.dot(B.T, temp)
-    #exp = np.exp(product)
-    #return exp / nlabels * exp
-
-
 def stable_softmax(x, A, B):
-    temp = sparse.csr_matrix.dot(A, x.T)  
-    X = np.dot(B, temp)
-    #print(X.shape)
+    temp = sparse.csr_matrix.dot(x, A)  
+    #print(temp)
+    X = np.dot(temp, B.T)
+    print(X)
     exps = np.exp(X - np.max(X))
-    return exps / np.sum(exps)
-    
-
-# calculates log-likelihood
-def log_likelihood(x, A, B, nlabels):
-    return np.log(softmax(x, A, B, nlabels))
+    #exps = np.exp(X)
+    #print(exps)
+    return (exps / np.sum(exps))[0]
 
 
 # finds the loss
-def loss_function(label, loglike):
-    return np.dot(label, loglike)
+def loss_function(x, A, B, label):
+    loglike = np.log(stable_softmax(x, A, B))
+    #print(loglike)
+    return np.dot(label, loglike.T)
 
 
 def main():
 
     # args from Simple Queries paper
     DIM=30
-    LR=0.1
+    LR=0.0001
     WORDGRAMS=2
     MINCOUNT=2
     MINN=3
@@ -92,37 +74,38 @@ def main():
     ##### instantiations #######################################
 
     # A
-    A_n = DIM               # rows
-    A_m = nwords + BUCKET   # cols
+    A_n = DIM               # cols
+    A_m = nwords + BUCKET   # rows
     uniform_val = 1.0 / DIM
-    A = np.random.uniform(-uniform_val, uniform_val, (A_n, A_m))
+    #A = np.random.uniform(-uniform_val, uniform_val, (A_m, A_n))
+    A = np.ones((A_m, A_n))
     #print(A.shape)
 
     # B
-    B_n = nlabels           # rows
-    B_m = DIM               # cols
-    B = np.zeros((B_n, B_m))
+    B_n = DIM               # cols
+    B_m = nlabels           # rows
+    B = np.zeros((B_m, B_n))
     #print(B.shape)
+
 
     #### train ################################################
 
     total_loss = 0
     #for i in range(EPOCH):
     # loop through each instance for SGD
+    alpha = LR
     loss = 0
     l = 0
     for x in input_:
-        #print(x.shape)
-        #loglike = log_likelihood(x, A, B, nlabels)
-        softmax = stable_softmax(x, A, B)
-        #print(softmax.shape)
-        #loss = loss_function(labels[l], loglike)
-        #total_loss += loss        
-
-        #B_new = update_b(B, A, x, labels[l], nlabels)
-        #temp = sparse.csr_matrix.dot(A, x.T)
-        #print(temp)
+        #softmax = stable_softmax(x, A, B)
+        label = labels[l]
+        B_old = B
         
+        loss = loss_function(x, A, B, label)
+        #print(loss)
+        B = gradient_B(B_old, A, x, label, nlabels, alpha)
+        total_loss += loss        
+
         l += 1
         
     
