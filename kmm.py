bb732ecb74c6d265ba0173f939aee50b01dd781f
madi <mcooley3@uwyo.edu>
2018-09-04 19:44:36 -0600

    added collect test set script

--- kmm.py
+++ kmm.py
@@ -3,6 +3,7 @@
 from dictionary3 import Dictionary
 import numpy as np
 from scipy import sparse
+import math
 
 
 def update_beta(X_train, X_test, beta, n_train, n_test):
@@ -39,17 +40,23 @@ def compute_k(n_train, n_test, X_train, X_test):
 def compute_ki(n_train, n_test, xi_train, X_test):
     _sum =  np.zeros((n_test))
     for j in range(n_test):
-        sum[j] = gaussian_kernel(xi_train, X_test[j], sigma)
+        _sum[j] = gaussian_kernel(xi_train, X_test[j], sigma)
         
     return n_train/n_test * _sum
 
+
+def compute_alpha(beta, _lambda, Y):
+    return (1/_lambda)*(np.dot(beta, Y) * (1/math.log(10)) + np.dot(beta, Y))
+    
 
 def gaussian_kernel(x_i, x_j, sigma):
     return  np.exp(-linalg.norm(x_i - x_j)**2 / (2 * (sigma ** 2)))
 
 
-def compute_loss():
-    return 1
+def compute_loss(K, beta, Y, alpha, _lambda):
+    return (np.dot(beta, Y) * np.log(np.exp(np.dot(alpha, K))
+                                    - np.dot(np.dot(beta, Y), K)
+                                    + (_lambda / 2) * np.dot(np.dot(alpha, alpha), K))
 
 
 def main():
