874efd42ea6afbb8c07fe32fa4f3d6b759e20c73
madi <mcooley3@uwyo.edu>
2018-12-09 10:22:25 -0700

    fjkdj

--- analyze_stats.py
+++ analyze_stats.py
@@ -19,7 +19,7 @@ def main():
     wftcfdir = '/project/lsrtwitter/mcooley3/output_data/wfasttext_cf/stats/'
     wftckdir = '/project/lsrtwitter/mcooley3/output_data/wfasttext_ck/stats/'
 
-    dataset = ['manual', 'train', 'test']
+    #dataset = ['manual', 'train', 'test']
     dirs = [ftdir, wftdir, wftcfdir, wftckdir]
 
     train_err = pd.DataFrame()
@@ -31,27 +31,47 @@ def main():
         num = 0
         
         for filenameft in os.listdir(d):
-            #for ds in dataset:
-                #if ds in filenameft and "error_" in filenameft:
+            print(filenameft)
             if "manual" in filenameft:
-                print(filenameft)
-                    
                 class_error = pd.read_csv(d+filenameft, sep=",", header=None)
                 print(class_error)
-                    
-                class_error = class_error.drop(class_error.columns[-1],axis=1)
-                summary = class_error.describe()
-                    mean = np.array(summary.loc[['mean']])
-                    std = np.array(summary.loc[['std']])
-                    mean.resize((EPOCH))
-                    std.resize((EPOCH))
-                    
-                    print()
+                man_err.concat(class_error)
+                
+            elif "test" in filenameft:
+                class_error = pd.read_csv(d+filenameft, sep=",", header=None)
+                #print(class_error)
+                test_err.concat(class_error)
             
-            #print("ERROR")
-            #print()
-            #print()
-            #print()
+            elif "train" in filenameft:
+                class_error = pd.read_csv(d+filenameft, sep=",", header=None)
+                #print(class_error)
+                train_err.concat(class_error)
+                    
+        # manual
+        man_err = man_err.drop(man_err.columns[-1],axis=1)
+        man_summary = man_err.describe()
+        man_mean = np.array(man_summary.loc[['mean']])
+        man_std = np.array(man_summary.loc[['std']])
+        man_mean.resize((EPOCH))
+        man_std.resize((EPOCH))
+        print(man_err)
+        
+        # test
+        test_err = test_err.drop(test_err.columns[-1],axis=1)
+        test_summary = test_err.describe()
+        test_mean = np.array(test_summary.loc[['mean']])
+        test_std = np.array(test_summary.loc[['std']])
+        test_mean.resize((EPOCH))
+        test_std.resize((EPOCH))
+        
+        # train
+        train_err = train_err.drop(train_err.columns[-1],axis=1)
+        train_summary = train_err.describe()
+        train_mean = np.array(train_summary.loc[['mean']])
+        train_std = np.array(train_summary.loc[['std']])
+        train_mean.resize((EPOCH))
+        train_std.resize((EPOCH))
+                
         
  
 if __name__ == '__main__':
